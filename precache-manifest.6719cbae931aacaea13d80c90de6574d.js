c1bbcb72091aae5f6c6cf6f0fbf356de35a23ae0
Alireza Mehrsina <mehr30na@gmail.com>
2018-11-25 17:37:49 +0330

    Updates

---
+++ precache-manifest.6719cbae931aacaea13d80c90de6574d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4bafef5c340d8e3f8d74cedd415a943",
+    "url": "/ReactPlaylist/static/media/bg.e4bafef5.jpg"
+  },
+  {
+    "revision": "0875b4d475fa38b785d4",
+    "url": "/ReactPlaylist/static/css/main.bd68809d.chunk.css"
+  },
+  {
+    "revision": "ffd749f8a7f09ddcd8c0",
+    "url": "/ReactPlaylist/static/js/1.ffd749f8.chunk.js"
+  },
+  {
+    "revision": "db54d75ba3fbe6370c1f",
+    "url": "/ReactPlaylist/static/js/runtime~main.db54d75b.js"
+  },
+  {
+    "revision": "bee6ec5dadef0ecbf4ac479a6a9b5a1c",
+    "url": "/ReactPlaylist/static/media/play.bee6ec5d.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactPlaylist/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0875b4d475fa38b785d4",
+    "url": "/ReactPlaylist/static/js/main.0875b4d4.chunk.js"
+  },
+  {
+    "revision": "122f38c5cc68f2b5a35649b43c1694eb",
+    "url": "/ReactPlaylist/static/media/play-button.122f38c5.svg"
+  },
+  {
+    "revision": "e48e19baa1bb577876ec379979f0fe89",
+    "url": "/ReactPlaylist/static/media/pause-button.e48e19ba.svg"
+  },
+  {
+    "revision": "4263659a2f0e0b3856702d4673801647",
+    "url": "/ReactPlaylist/static/media/mute.4263659a.svg"
+  },
+  {
+    "revision": "3a7c5aa9c7b935940dc709916f18ade9",
+    "url": "/ReactPlaylist/static/media/speaker.3a7c5aa9.svg"
+  },
+  {
+    "revision": "75cf3cbfd1a0e0cbbcda5f7009fea26b",
+    "url": "/ReactPlaylist/static/media/bgtv.75cf3cbf.jpg"
+  },
+  {
+    "revision": "28f0b4977d2e551280ca641371ca2173",
+    "url": "/ReactPlaylist/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.595313712de37fb6c46fdbd7936a015e.js"
+  "/ReactPlaylist/precache-manifest.6719cbae931aacaea13d80c90de6574d.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("./index.html", {
+workbox.routing.registerNavigationRoute("/ReactPlaylist/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
