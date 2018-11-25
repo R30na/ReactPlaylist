5a0cc2f2aae20ed1b2ce08afe1cb72d8d3f29200
Alireza Mehrsina <mehr30na@gmail.com>
2018-11-25 06:18:48 +0330

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "playlistdemo/precache-manifest.0aadab5792aab066555b84cae78c5600.js"
+  "/playlistdemo/precache-manifest.c4f8ec162515e7773c33ffe4e7490343.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("playlistdemo/index.html", {
+workbox.routing.registerNavigationRoute("/playlistdemo/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
